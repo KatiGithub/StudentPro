bf346223c4ba6ef58e20569011c0f9182d8de347
Kati Pairojtanachai <kati.pairojtanachai@gmail.com>
2023-03-31 23:43:58 +0200

    Got BlocConsumer for welcome_screen.dart to work

--- customer_frontend/lib/views/main.dart
+++ customer_frontend/lib/views/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:studio_projects/views/login/login_screen.dart';
+import 'package:studio_projects/views/authentication/login/login_screen.dart';
 import 'package:studio_projects/views/welcome/welcome_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import '../firebase_options.dart';
--- customer_frontend/lib/views/welcome/welcome_screen.dart
+++ customer_frontend/lib/views/welcome/welcome_screen.dart
@@ -1,7 +1,7 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:studio_projects/views/login/login_screen.dart';
+import 'package:studio_projects/views/authentication/login/login_screen.dart';
 import 'package:studio_projects/views/welcome/welcome_bloc.dart';
 import 'package:studio_projects/views/welcome/welcome_events.dart';
 import 'package:studio_projects/views/welcome/welcome_state.dart';
@@ -17,45 +17,51 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
     return BlocProvider<WelcomeBloc>(
-        create: (BuildContext context) => WelcomeBloc(),
-        child: Scaffold(
-            body: BlocConsumer<WelcomeBloc, WelcomeState>(
-              bloc: BlocProvider.of<WelcomeBloc>(context),
-                listener: (context, state) {
+      create: (BuildContext context) => WelcomeBloc(),
+      child: BlocConsumer<WelcomeBloc, WelcomeState>(
+        listener: (context, state) {
+          print(state);
           if (state is WelcomeLoginNotDone) {
-            // Navigator.pushReplacementNamed(context, LoginScreen.id);
+            Navigator.pushReplacementNamed(context, LoginScreen.id);
             print("Not logged in");
           } else if (state is WelcomeLoginDone) {
             print("Logged in. Redirecting to Home Page");
           }
-        }, builder: (context, state) {
-          return Padding(
-            padding: EdgeInsets.symmetric(horizontal: 24.0),
-            child: Center(
-                child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                AnimatedTextKit(animatedTexts: [
-                  TypewriterAnimatedText('StudentPro',
-                      textStyle: const TextStyle(
-                          fontSize: 40.0, fontWeight: FontWeight.w900))
-                ],
+        },
+        builder: (context, state) {
+          return Scaffold(
+            body: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24.0),
+              child: Center(
+                  child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  AnimatedTextKit(
+                    animatedTexts: [
+                      TypewriterAnimatedText('StudentPro',
+                          textStyle: const TextStyle(
+                              fontSize: 40.0, fontWeight: FontWeight.w900))
+                    ],
                     repeatForever: false,
-                onFinished: () {
-                  print("Finished Playing");
-                  WelcomeBloc().add(OnFinishedPlaying());
-                  print("Moving on");
-                },),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Container(
-                  child: Image.asset('images/logo.png'),
-                  height: 60.0,
-                )
-              ],
-            )),
+                    onFinished: () {
+                      print("Finished Playing");
+                      context.read<WelcomeBloc>().add(OnFinishedPlaying());
+                      print("Moving on");
+                    },
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Container(
+                    child: Image.asset('images/logo.png'),
+                    height: 60.0,
+                  )
+                ],
+              )),
+            ),
           );
-        })));
+        },
+      ),
+    );
   }
 }
