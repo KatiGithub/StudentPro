fa2a184c2a8831b69904798b5ad6d6a79de6b7e7
Kati Pairojtanachai <kati.pairojtanachai@gmail.com>
2023-08-02 09:38:10 +0700

    added all request endpoints to BusinessUserController

--- backend/mahapro/src/main/java/com/mahapro/backend/mahapro/controller/v1/business_controller/BusinessUserController.java
+++ backend/mahapro/src/main/java/com/mahapro/backend/mahapro/controller/v1/business_controller/BusinessUserController.java
@@ -1,13 +1,41 @@
 package com.mahapro.backend.mahapro.controller.v1.business_controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/business/user/")
 public class BusinessUserController {
+
+    @GetMapping("/login")
     public ResponseEntity<String> login() {
+        return ResponseEntity.ok("");
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser() {
+        return ResponseEntity.ok("");
+    }
+
+    @PostMapping("/add_user")
+    public ResponseEntity addNewBusinessUser() {
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteUserFromBusiness() {
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @GetMapping("/all/{id}")
+    public ResponseEntity<String> getBusinessUsersFromBusiness() {
+        return ResponseEntity.ok("");
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<String> getBusinessUserFromCode(@RequestParam("code") String code) {
         return ResponseEntity.ok("");
     }
 }
